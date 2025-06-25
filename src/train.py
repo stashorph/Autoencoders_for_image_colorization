92575bbc44242dc234cfe9218fd78b913e25b951
stashorph <saieshjitendra@gmail.com>
2025-06-25 15:08:44 +0530

    Training script for classification model.

--- src/train.py
+++ src/train.py
@@ -1,6 +1,3 @@
-import config
-from data_loader import get_dataset
-from model import get_compiled_model
 import tensorflow as tf
 from tensorflow.keras.callbacks import ( #type: ignore
     ModelCheckpoint,
@@ -9,15 +6,19 @@ from tensorflow.keras.callbacks import ( #type: ignore
 )
 import matplotlib.pyplot as plt
 
+import config
+from data_loader import get_dataset
+from model import get_compiled_model
+
 def run_training():
 
-    print("Starting Autoencoder Model Training")
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
 
-    gpu = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(gpu, True)
 
-    train_dataset = get_dataset('train')
-    val_dataset = get_dataset('validation')
+    train_dataset = get_dataset(split="train")
+    val_dataset = get_dataset(split="validation")
     print("Datasets loaded.\n")
 
     # Build and compile the model
@@ -28,7 +29,6 @@ def run_training():
         filepath=config.BEST_MODEL_PATH,
         monitor='val_loss',
         save_best_only=True,
-        save_weights_only=False,
         mode='min',
         verbose=1
     )
@@ -48,51 +48,41 @@ def run_training():
         mode='min'
     )
 
-    callbacks = [
-        model_checkpoint,
-        early_stop,
-        lr_reduce
-    ]
-
     # Training
-    print(f"\nStarting training for {config.EPOCHS} epochs with batch size {config.BATCH_SIZE}")
     history = model.fit(
         train_dataset,
         epochs=config.EPOCHS,
         validation_data=val_dataset,
-        callbacks=callbacks
+        callbacks= [
+        model_checkpoint,
+        early_stop,
+        lr_reduce
+    ]
     )
     print("\n Training Finished ")
 
     # Save the final model and plot
-    print(f"Saving final model to '{config.FINAL_MODEL_PATH}'...")
     model.save(config.FINAL_MODEL_PATH)
     
-    plot(history)
+    plot_training_history(history)
     
 
-def plot(history):
+def plot_training_history(history):
     
-    plt.style.use("fivethirtyeight ")
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 6))
+    plt.style.use("fivethirtyeight")
+    plt.figure(figsize=(12, 5))
 
-    # Plot training/validation loss
-    ax1.plot(history.history['loss'], label='Training loss', color='blue')
-    ax1.plot(history.history['val_loss'], label='Val loss', color='orange')
-    ax1.set_title('Loss vs. Epochs')
-    ax1.set_xlabel('Epochs')
-    ax1.set_ylabel('Loss (MSE)')
-    ax1.legend()
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Val Loss')
+    plt.legend(), plt.title('Loss')
 
-    # Plot mae
-    ax2.plot(history.history['mae'], label='Train MAE', color='green')
-    ax2.plot(history.history['val_mae'], label='Val MAE', color='red')
-    ax2.set_title('MAE vs. Epochs')
-    ax2.set_xlabel('Epochs')
-    ax2.set_ylabel('MAE')
-    ax2.legend()
-    
-    fig.suptitle('Model Training History')
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['sparse_categorical_accuracy'], label='Train Acc')
+    plt.plot(history.history['val_sparse_categorical_accuracy'], label='Val Acc')
+    plt.legend(), plt.title('Accuracy')
+
+    plt.tight_layout()
     plt.savefig(config.TRAINING_PLOT_PATH)
 
 if __name__ == '__main__':
